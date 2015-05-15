9993ced98e7566be02607c17d1fcc031c95230d8
jloyde100 <john.loyde@zeltontech.com>
2015-05-15 06:59:04 -0700

    Added code for connecting to weblogic in the zatmain.py

--- zat.sh
+++ zat.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo "Welcome to ZAT"
+
+if [ -z "$WL_HOME" ]; then
+   echo "You environment is not set properly."
+   echo "Please source the setWLSEnv.sh file that comes with weblogic"
+else 
+   $WL_HOME/common/bin/wlst.sh zatmain.py
+fi
--- zatmain.py
+++ zatmain.py
@@ -1,2 +1,14 @@
+import os
 
-print 'Main Menu'
+myurl="t3://localhost:7001"
+
+if not os.path.exists("wl.py"):
+   writeIniFile("wl.py")
+
+if os.path.exists("configfile.secure"):
+   connect(userConfigFile="./configfile.secure", userKeyFile="./keyfile.secure", url=myurl) 
+else:
+   connect()
+   storeUserConfig("./configfile.secure","./keyfile.secure")
+
+exit()
